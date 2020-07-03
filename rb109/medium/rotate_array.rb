81285affd4d5983ff358ac0190b8d142164dab13
stevenalston <salston@eliteclientele.com>
2020-07-03 10:38:04 -0500

    rotate array part 2 complete

--- rb109/medium/rotate_array.rb
+++ rb109/medium/rotate_array.rb
@@ -35,10 +35,104 @@ def rotate_array(array)
   arr << first_el
 end
 
-  p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
-  p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
-  p rotate_array(['a']) == ['a']
+# lachool solution
+def rotate_array_ls(array)
+  array[1..-1] + [array[0]]
+end
 
-  x = [1, 2, 3, 4]
-  p rotate_array(x) == [2, 3, 4, 1]   # => true
-  p x == [1, 2, 3, 4]  
+rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+rotate_array(['a']) == ['a']
+x = [1, 2, 3, 4]
+rotate_array(x) == [2, 3, 4, 1]   # => true
+x == [1, 2, 3, 4]  
+
+=begin
+  P: define a method which takes 2 args. The first arg is an Integer, the second in also an Integer
+  which specifies how many of the right most digits should be rotated. 
+
+  'n' is always a positive integer
+  E:
+  rotate_rightmost_digits(735291, 1) == 735291
+  rotate_rightmost_digits(735291, 2) == 735219
+  rotate_rightmost_digits(735291, 3) == 735912
+  rotate_rightmost_digits(735291, 4) == 732915
+  rotate_rightmost_digits(735291, 5) == 752913
+  rotate_rightmost_digits(735291, 6) == 352917
+  D:
+
+    Input: (int, int)
+
+    Output: int
+
+  A:
+
+    SET- local var 'arr' to the digits of the Integer
+      - reverse arr
+    SET - local var 'd' = REMOVE digit 'n'
+    APPEND - removed digit to the end of 'arr'
+    RETURN - arr as an Integer
+
+  C:
+=end
+
+def rotate_rightmost_digits(int, n)
+  arr = int.digits.reverse
+  d = arr.delete_at(-n)
+  (arr << d).join.to_i
+end
+
+rotate_rightmost_digits(735291, 1) == 735291
+rotate_rightmost_digits(735291, 2) == 735219
+rotate_rightmost_digits(735291, 3) == 735912
+rotate_rightmost_digits(735291, 4) == 73
+rotate_rightmost_digits(735291, 5) == 752913
+rotate_rightmost_digits(735291, 6) == 352917
+
+# Lschool solution
+def rotate_rightmost_digits_ls(number, n)
+  all_digits = number.to_s.chars
+  all_digits[-n..-1] = rotate_array_ls(all_digits[-n..-1])
+  all_digits.join.to_i
+end
+
+=begin
+
+  P: If you take a number like 735291, and rotate it to the left, you get 352917. If you now keep the first digit fixed in
+  place, and rotate the remaining digits, you get 329175. Keep the first 2 digits fixed in place and rotate again to
+  321759. Keep the first 3 digits fixed in place and rotate again to get 321597. Finally, keep the first 4 digits fixed in
+  place and rotate the final 2 digits to get 321579. The resulting number is called the maximum rotation of the original
+  number.
+
+  Write a method that takes an integer as argument, and returns the maximum rotation of that argument. You can (and probably should) use the rotate_rightmost_digits method from the previous exercise.
+  
+  Note that you do not have to handle multiple 0s.
+
+  E:
+  max_rotation(735291) == 321579
+  max_rotation(3) == 3
+  max_rotation(35) == 53
+  max_rotation(105) == 15 # the leading zero gets dropped
+  max_rotation(8_703_529_146) == 7_321_609_845
+
+  D:
+
+    Input: Integer
+
+    Output: Integer
+
+  A:
+
+    SET-
+
+    ITERATE-
+
+    IF-
+
+    ELSE-
+
+    PRINT-
+
+  C:
+
+=end
